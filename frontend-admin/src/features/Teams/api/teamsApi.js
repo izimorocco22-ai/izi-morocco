95f04e88b793090ba735b800d6352ca39d7c8955
mjrajput04 <manojvagadiya04@gmail.com>
2026-02-21 17:02:36 +0530

    fix

--- frontend-admin/src/features/Teams/api/teamsApi.js
+++ frontend-admin/src/features/Teams/api/teamsApi.js
@@ -1,12 +1,15 @@
-import axiosInstance from '../../../utils/axios'
+import { callAPI } from '../../../services/callApi'
 
 export const fetchTeams = async () => {
-  const res = await axiosInstance.get('/teams')
-  return res.data?.data || []
+  const res = await callAPI('/teams', {
+    method: 'GET',
+  })
+  return res.data || []
 }
 
 export const fetchTeamWithPlayers = async (id) => {
-  const res = await axiosInstance.get(`/teams/${id}`)
-  return res.data?.data
+  const res = await callAPI(`/teams/${id}`, {
+    method: 'GET',
+  })
+  return res.data
 }
-
